9e6f399e8bdaf9101eaea720507e44bd80b5a08e
Batbakarrik <batbakarrik@outlook.com>
2020-06-21 09:33:23 +0200

    Nouvelle structure

--- App.js
+++ App.js
@@ -1,146 +1,80 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
-import MainTabNavigator from './MainTabNavigator'
+import Home from './screens/Home'
+import Loading from './screens/Loading'
+import Login from './screens/Login'
+import Signup from './screens/Signup'
+import Courant from './screens/Courant'
+import Tension from './screens/Tension'
 
-export default class App extends Component {
-    // constructor(props) {
-    //     super(props);
-    //         this.state= {
-    //             prim_I: 0,
-    //             second_I: 5,
-    //             prim_U: 0,
-    //             second_U: 110,
-    //             coef_I: 0,
-    //             coef_U: 0,
-    //             seuil_I: 0,
-    //             seuil_U: 0,
-    //             seuil_V: 0,
-    //             inj_I: 0,
-    //             inj_I1: 0,
-    //             inj_I2: 0,
-    //             inj_U: 0,
-    //             inj_V: 0,
-    //             maj: false,
-    //             curve: 0,
-    //             k: 1,
-    //             temps1: 0,
-    //             temps2: 0,
-    //             temps3: 0,
-    //             seuil_Ix1: 0,
-    //             seuil_Ix2: 0,
-    //             seuil_Ix3: 0,
-    //         }
-    //     this.calc_seuilI = this.calc_seuilI.bind(this);
-    //     this.calc_seuilISIT = this.calc_seuilISIT.bind(this);
-    //     this.calc_seuilIVIT = this.calc_seuilIVIT.bind(this);
-    //     this.calc_seuilIEIT = this.calc_seuilIEIT.bind(this);
-    //     this.calc_seuilU = this.calc_seuilU.bind(this);
-    //     }
+import * as firebase from 'firebase'
+import colors from './src/utils/colors'
 
-    //     calc_seuilI = async() => {
-    //         await this.setState({coef_I: this.state.second_I / this.state.prim_I})
-    //         await this.setState({inj_I: this.state.seuil_I * this.state.coef_I * 0.95})
-    //         await this.setState({inj_I1: (this.state.seuil_I * this.state.coef_I) * 1})
-    //         await this.setState({inj_I2: (this.state.seuil_I * this.state.coef_I) * 1.1})
-    //     }
-    //     calc_seuilISIT = async() => {
-    //         await this.setState({coef_I: this.state.second_I / this.state.prim_I})
-    //         await this.setState({inj_I: (this.state.seuil_I * this.state.coef_I) * this.state.seuil_Ix1})
-    //         await this.setState({inj_I1: (this.state.seuil_I * this.state.coef_I) * this.state.seuil_Ix2})
-    //         await this.setState({inj_I2: (this.state.seuil_I * this.state.coef_I) * this.state.seuil_Ix3})
-    //         await this.setState({temps1: ((this.state.k/2.97)*(0.14/((Math.pow((this.state.inj_I),0.02))-1)))})
-    //         await this.setState({temps2: ((this.state.k/2.97)*(0.14/((Math.pow((this.state.inj_I1),0.02))-1)))})
-    //         await this.setState({temps3: ((this.state.k/2.97)*(0.14/((Math.pow((this.state.inj_I2),0.02))-1)))})
-    //     }
-    //     calc_seuilIVIT = async() => {
-    //         await this.setState({coef_I: this.state.second_I / this.state.prim_I})
-    //         await this.setState({inj_I: (this.state.seuil_I * this.state.coef_I) * this.state.seuil_Ix1})
-    //         await this.setState({inj_I1: (this.state.seuil_I * this.state.coef_I) * this.state.seuil_Ix2})
-    //         await this.setState({inj_I2: (this.state.seuil_I * this.state.coef_I) * this.state.seuil_Ix3})
-    //         await this.setState({temps1: ((this.state.k/1.5)*(13.5/((Math.pow((this.state.inj_I),1))-1)))})
-    //         await this.setState({temps2: ((this.state.k/1.5)*(13.5/((Math.pow((this.state.inj_I1),1))-1)))})
-    //         await this.setState({temps3: ((this.state.k/1.5)*(13.5/((Math.pow((this.state.inj_I2),1))-1)))})
-    //     }
-    //     calc_seuilIEIT = async() => {
-    //         await this.setState({coef_I: this.state.second_I / this.state.prim_I})
-    //         await this.setState({inj_I: (this.state.seuil_I * this.state.coef_I) * this.state.seuil_Ix1})
-    //         await this.setState({inj_I1: (this.state.seuil_I * this.state.coef_I) * this.state.seuil_Ix2})
-    //         await this.setState({inj_I2: (this.state.seuil_I * this.state.coef_I) * this.state.seuil_Ix3})
-    //         await this.setState({temps1: ((this.state.k/0.808)*(80/((Math.pow((this.state.inj_I),2))-1)))})
-    //         await this.setState({temps2: ((this.state.k/0.808)*(80/((Math.pow((this.state.inj_I1),2))-1)))})
-    //         await this.setState({temps3: ((this.state.k/0.808)*(80/((Math.pow((this.state.inj_I2),2))-1)))})
-    //     }
-    //     calc_seuilU = async() => {
-    //         await this.setState({coef_U: this.state.second_U / this.state.prim_U})
-    //         await this.setState({inj_U: (this.state.seuil_U * this.state.coef_U)})
-    //         await this.setState({inj_V: (this.state.seuil_V * this.state.coef_U) / 1.732 })
-    //     }
-    //     calc_Surch = async() => {
-    //         await this.setState({Surch: math.log((math.pow((1),2)-(math.pow(1),2))/math.pow((1),2)-1)})
-    //     }
-    //     setPrim_I = (prim_I) => {
-    //         this.setState({prim_I: prim_I})
-    //     }
-    //     setSecond_I = (second_I) => {
-    //         this.setState({second_I: second_I})
-    //     }
-    //     setPrim_U = (prim_U) => {
-    //         this.setState({prim_U: prim_U})
-    //     }
-    //     setSecond_U = (second_U) => {
-    //         this.setState({second_U: second_U})
-    //     }
-    //     setSeuil_I = (seuil_I) => {
-    //         this.setState({seuil_I: seuil_I})
-    //     }
-    //     setSeuil_U = (seuil_U) => {
-    //         this.setState({seuil_U: seuil_U})
-    //     }
-    //     setSeuil_V = (seuil_V) => {
-    //         this.setState({seuil_V: seuil_V})
-    //     }
-    //     setCurve = (curve) => {
-    //         this.setState({curve: curve})
-    //     }
-    //     setk = (k) => {
-    //         this.setState({k: k})
-    //     }
-    //     setSeuil_Ix1 = (seuil_Ix1) => {
-    //         this.setState({seuil_Ix1: seuil_Ix1})
-    //         console.log("setSeuil_I")
-    //     }
-    //     setSeuil_Ix2 = (seuil_Ix2) => {
-    //         this.setState({seuil_Ix2: seuil_Ix2})
-    //     }
-    //     setSeuil_Ix3 = (seuil_Ix3) => {
-    //         this.setState({seuil_Ix3: seuil_Ix3})
-    //     }
-    render() {
-        // console.log({...this.state})
-        return (
-            <MainTabNavigator
-                // screenProps = {{
-                //     ...this.state,
-                //     setPrim_I : this.setPrim_I,
-                //     setSecond_I : this.setSecond_I,
-                //     setPrim_U : this.setPrim_U,
-                //     setSecond_U : this.setSecond_U,
-                //     setSeuil_I : this.setSeuil_I,
-                //     setSeuil_U : this.setSeuil_U,
-                //     setSeuil_V : this.setSeuil_V,
-                //     calc_coef : this.calc_coef,
-                //     calc_seuilI : this.calc_seuilI,
-                //     calc_seuilISIT : this.calc_seuilISIT,
-                //     calc_seuilIVIT : this.calc_seuilIVIT,
-                //     calc_seuilIEIT : this.calc_seuilIEIT,
-                //     calc_seuilU : this.calc_seuilU,
-                //     setCurve : this.setCurve,
-                //     setk : this.setk,
-                //     setSeuil_Ix1 : this.setSeuil_Ix1,
-                //     setSeuil_Ix2 : this.setSeuil_Ix2,
-                //     setSeuil_Ix3 : this.setSeuil_Ix3,
-                // }}
-            />
-        )
+var firebaseConfig = {
+    apiKey: "AIzaSyCaVkEUsqPO-eGbeZQ7ubhQLwKkylI5hf8",
+    authDomain: "spie-mob.firebaseapp.com",
+    databaseURL: "https://spie-mob.firebaseio.com",
+    projectId: "spie-mob",
+    storageBucket: "spie-mob.appspot.com",
+    messagingSenderId: "376115758009",
+    appId: "1:376115758009:web:b4916082028469ae326c68"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+const AppTabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        tabBarIcon: () => <Ionicons name="ios-home" size={24} color={'white'}></Ionicons>
+      }
+    },
+    Courant: {
+      screen: Courant,
+      navigationOptions: {
+        tabBarIcon: () => <Ionicons name="ios-calculator" size={24} color={'white'}></Ionicons>
+      }
+    },
+    Tension: {
+      screen: Tension,
+      navigationOptions: {
+        tabBarIcon: () => <Ionicons name="ios-calculator" size={24} color={'white'}></Ionicons>
+      }
+    },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarOptions: {
+        activeTintColor: 'white',
+        inactiveTintColor: 'gray',
+        activeBackgroundColor: colors.backgroundactive,
+        inactiveBackgroundColor: colors.background,
+      },
     }
-}
+    )
+  }
+)
+
+const AuthStack = createStackNavigator ({
+  Login: Login,
+  Signup: Signup,
+});
+
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      Loading: Loading,
+      App: AppTabNavigator,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "Loading"
+    }
+  )
+)
--- screens/Courant_old.js
+++ screens/Courant_old.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { Component } from 'react'
 import { StyleSheet, View, TouchableOpacity, Text, Picker } from 'react-native'
 
 import colors from '../src/utils/colors'
@@ -6,88 +6,20 @@ import Input from '../src/components/Input'
 import InputSeuil from '../src/components/InputSeuil'
 import Injecter from '../src/components/Injecter'
 
-const Courant_test = () => {
+export default class Courant extends Component{
+    static navigationOptions = {
+        title: 'Courant',
+      }
 
-        const [seuil_I, setseuil_I] = useState(0);
-        const [prim_I, setprim_I] = useState(0);
-        const [second_I, setsecond_I] = useState(5);
-        const [curve, setcurve] = useState(0);
-        const [k, setk] = useState(0);
-        const [seuil_Ix1, setseuil_Ix1] = useState(0);
-        const [seuil_Ix2, setseuil_Ix2] = useState(0);
-        const [seuil_Ix3, setseuil_Ix3] = useState(0);
-        const [coef_I, setcoef_I] = useState(0);
-        const [inj_I, setinj_I] = useState(0);
-        const [inj_I1, setinj_I1] = useState(0);
-        const [inj_I2, setinj_I2] = useState(0);
-        const [temps1, settemps1] = useState(0);
-        const [temps2, settemps2] = useState(0);
-        const [temps3, settemps3] = useState(0);
-
-        const calc_seuilI = () => {
-            setinj_I(seuil_I.Value * coef_I * 0.95)
-            setinj_I1((seuil_I.Value * coef_I) * 1)
-            setinj_I2((seuil_I.Value * coef_I) * 1.1)
-        };
-        const calc_seuilISIT = () => {
-            setcoef_I(second_I / prim_I)
-            setinj_I((seuil_I * coef_I) * seuil_Ix1)
-            setinj_I1((seuil_I * coef_I) * seuil_Ix2)
-            setinj_I2((seuil_I * coef_I) * seuil_Ix3)
-            settemps1(((k/2.97)*(0.14/((Math.pow((inj_I),0.02))-1))))
-            settemps2(((k/2.97)*(0.14/((Math.pow((inj_I1),0.02))-1))))
-            settemps3(((k/2.97)*(0.14/((Math.pow((inj_I2),0.02))-1))))
-        };
-        const calc_seuilIVIT = () => {
-            setcoef_I(second_I / prim_I)
-            setinj_I((seuil_I * coef_I) * seuil_Ix1)
-            setinj_I1((seuil_I * coef_I) * seuil_Ix2)
-            setinj_I2((seuil_I * coef_I) * seuil_Ix3)
-            settemps1(((k/1.5)*(13.5/((Math.pow((inj_I),1))-1))))
-            settemps2(((k/1.5)*(13.5/((Math.pow((inj_I1),1))-1))))
-            settemps3(((k/1.5)*(13.5/((Math.pow((inj_I2),1))-1))))
-        };
-        const calc_seuilIEIT = () => {
-            setcoef_I(second_I / prim_I)
-            setinj_I((seuil_I * coef_I) * seuil_Ix1)
-            setinj_I1((seuil_I * coef_I) * seuil_Ix2)
-            setinj_I2((seuil_I * coef_I) * seuil_Ix3)
-            settemps1(((k/0.808)*(80/((Math.pow((inj_I),2))-1))))
-            settemps2(((k/0.808)*(80/((Math.pow((inj_I1),2))-1))))
-            settemps3(((k/0.808)*(80/((Math.pow((inj_I2),2))-1))))
-        };
-
-        useEffect(() => {
-            setseuil_I(0)
-            setprim_I(0)
-            setsecond_I(5)
-            setcurve(0)
-            setk(0)
-            setseuil_Ix1(0)
-            setseuil_Ix2(0)
-            setseuil_Ix3(0)
-            setcoef_I(0)
-            setinj_I(0)
-            setinj_I1(0)
-            setinj_I2(0)
-            settemps1(0)
-            settemps2(0)
-            settemps3(0)
-        }, [])
-
-        useEffect(() => {
-            setcoef_I(second_I / prim_I.Value)
-        }, [prim_I, second_I])
-        
-        
-        console.log({prim_I, second_I, seuil_I})
+    render () {
+        const {seuil_I, setSeuil_I, setPrim_I, setSecond_I, prim_I, second_I, calc_seuilI, calc_seuilISIT,calc_seuilIVIT, calc_seuilIEIT, inj_I, inj_I1, inj_I2, curve, setCurve, k, setk, temps1, temps2, temps3, seuil_Ix1, seuil_Ix2, seuil_Ix3, setSeuil_Ix1, setSeuil_Ix2, setSeuil_Ix3} =  this.props.screenProps
             return (
                 <View style={styles.container}>
                     <View style={styles.container2}>
                         <Text style={styles.text2}>Caractéristiques TC</Text>
                         <View style={styles.container1}>
                             <View style={styles.container2}>
-                                <Input Title="Primaire TC" Change={(e) => setprim_I({Value:e})} Value={prim_I} Length={4} Placeholder="Entrez Valeur"/>
+                                <Input Title="Primaire TC" Change={setPrim_I} Value={prim_I} Length={4} Placeholder="Entrez Valeur"/>
                             </View>
                             <View style={styles.container2}>
                             <Text style={styles.text1}>Secondaire TC</Text>
@@ -95,7 +27,7 @@ const Courant_test = () => {
                                     <Picker
                                         mode = "dropdown"
                                         selectedValue = {second_I}
-                                        onValueChange = {setsecond_I}
+                                        onValueChange = {setSecond_I}
                                         style = {styles.text3}>
                                         <Picker.Item label = "5A" value ="5"/>
                                         <Picker.Item label = "1A" value ="1"/>
@@ -111,7 +43,7 @@ const Courant_test = () => {
                                     <Picker
                                         mode = "dropdown"
                                         selectedValue = {curve}
-                                        onValueChange = {setcurve}
+                                        onValueChange = {setCurve}
                                         style = {styles.text3}>
                                         <Picker.Item label = "DT" value = "0"/>
                                         <Picker.Item label = "CEI SIT" value = "1"/>
@@ -121,21 +53,21 @@ const Courant_test = () => {
                                 </View>
                             </View>
                             <View style={styles.container2}>
-                                <Input Title="I_Seuil (A)" Change={(e) => setseuil_I({Value:e})} Valeur={seuil_I} Length={4} Placeholder="Entrez Seuil"/>
+                                <Input Title="I_Seuil (A)" Change={setSeuil_I} Valeur={seuil_I} Length={4} Placeholder="Entrez Seuil"/>
                             </View>
                             <View style={styles.container2}>
-                                {curve == '0'? null : <Input Title="Seuil k" Change={(e) => setk({Value:e})} Valeur={k} Length={4} Placeholder="Entrez Seuil"/>}
+                                {curve !== '0'? <Input Title="Seuil k" Change={setk} Valeur={k} Length={4} Placeholder="Entrez Seuil"/>: null}
                             </View>
                         </View>
                     </View>
                     <Text style={styles.text2}>Résultats</Text>
                     <View style={styles.container1}>
-                        {curve == '0'? null : <InputSeuil seuil_Ix1={seuil_Ix1} setseuil_Ix1={setseuil_Ix1} seuil_Ix2={seuil_Ix2} setseuil_Ix2={setseuil_Ix2} seuil_Ix3={seuil_Ix3} setseuil_Ix3={setseuil_Ix3} curve={curve}/>}
+                        <InputSeuil seuil_Ix1={seuil_Ix1} setSeuil_Ix1={setSeuil_Ix1} seuil_Ix2={seuil_Ix2} setSeuil_Ix2={setSeuil_Ix2} seuil_Ix3={seuil_Ix3} setSeuil_Ix3={setSeuil_Ix3} curve={curve}/>
                         <Injecter Title1="0.95 x Is" Title1b="x Is" inj1={inj_I} Title2="1 x Is" Title2b="x Is" inj2={inj_I1} Title3="1.1 x Is" Title3b="x Is" inj3={inj_I2} curve={curve}/>
-                        {curve == '0'? null : <Injecter Title1="x1" Title1b="tps (s)" inj1={temps1} Title2="x2" Title2b="tps (s)" inj2={temps2} Title3="x3" Title3b="tps (s)" inj3={temps3} curve={curve}/>}
+                        {curve === '0'? null : <Injecter Title1="x1" Title1b="tps (s)" inj1={temps1} Title2="x2" Title2b="tps (s)" inj2={temps2} Title3="x3" Title3b="tps (s)" inj3={temps3} curve={curve}/>}
                     </View>
                     <View style={styles.container1}>
-                        {curve =='0' ?
+                        {curve=='0' ?
                             <TouchableOpacity
                                 style={{activeOpacity:2}}
                                 onPress={calc_seuilI}
@@ -163,14 +95,12 @@ const Courant_test = () => {
                                 >
                                 <Text style={styles.button}>Calculer</Text>
                                 </TouchableOpacity>: null
-                        }
+                    }
                     </View>
                 </View>
-            )
-};
-
-export default Courant_test
-
+        )
+    }
+}
 const styles = StyleSheet.create({
     container: {
         display: 'flex',
