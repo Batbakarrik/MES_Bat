3a6efa371f24dceca37cb694c08e963c80607e7a
Batbakarrik <batbakarrik@outlook.com>
2020-07-30 09:17:43 +0200

    VisualStudioEmulator

--- screens/Tension.js
+++ screens/Tension.js
@@ -10,51 +10,62 @@ const Tension = () => {
     const [seuil_U, setseuil_U] = useState(0);
     const [seuil_V, setseuil_V] = useState(0);
     const [prim_U, setprim_U] = useState(0);
-    const [second_U, setsecond_U] = useState(0);
+    const [second_U, setsecond_U] = useState(100);
     // const [calc_seuilU, setcalc_seuilU] = useState(0);
     const [coef_U, setcoef_U] = useState(0);
     const [inj_U, setinj_U] = useState(0);
     const [inj_V, setinj_V] = useState(0);
 
-    const calc_USimple = () => {
-        seuil_U(prim_U/1.732)
-    };
+    // const calc_USimple = () => {
+    //     seuil_U(prim_U/1.732)
+    // };
+
     const calc_seuilU = () => {
-        seuil_U(prim_U/1.732)
+        setinj_U(seuil_U * coef_U)
     };
 
+    
+
     useEffect(() => {
-        setcoef_U(second_U / prim_U.Value)},[prim_U, second_U]);
+        setcoef_U(second_U / prim_U)
+    },[prim_U, second_U]);
+
+    console.log({prim_U, second_U, seuil_U, coef_U})
 
          return (
             <View style={styles.container}>
                 <View style={styles.container2}>
                     <Text style={styles.text2}>Caractéristiques TP</Text>
                         <View style={styles.container1}>
-                            <Input Title="Primaire TP" Change={setprim_U} Value={prim_U} Length={6} Placeholder="Entrez Valeur (V)"/>
+                            <View style={styles.container2}>
+                                <Input Title="Primaire TP" Change={setprim_U} Value={prim_U} Length={6} Placeholder="Entrez Valeur"/>
+                            </View>
                             <View style={styles.container2}>
                             <Text style={styles.text1}>Secondaire TP</Text>
                                 <View style={styles.text4}>
                                     <Picker
                                         mode = "dropdown"
                                         selectedValue = {second_U}
-                                        onValueChange = {setsecond_U}
+                                        onValueChange = {newsecond_U =>setsecond_U(newsecond_U)}
                                         style = {styles.text3}>
                                         <Picker.Item label = "100V" value ="100"/>
                                         <Picker.Item label = "110V" value ="110"/>
                                     </Picker>
                                 </View>
                             </View>
+                            <View style={styles.container2}>
+                                <Input Title="Coef_U" Placeholder={coef_U.toFixed(3)} Length={4}/>
+                            </View>
                         </View>
                 </View>
-                <View style={styles.container1}>
-                    <Input Title="Seuil V" Change={setseuil_V} Value={seuil_V} Length={6} Placeholder="Entrez Valeur (V)"/>
+                {/* <View style={styles.container1}>
+                    <Input Title="Seuil V" Change={setseuil_V} Value={seuil_V} Length={6} Placeholder="Entrez Valeur"/>
                     <View style={styles.container2}>
                         <Seuil Title="Injecter" Value={inj_V.toFixed(2)}/>
                     </View>
-                </View>
+                </View> */}
                 <View style={styles.container1}>
-                    <Input Title="Seuil U" Change={setseuil_U} Value={seuil_U} Length={6} Placeholder="Entrez Valeur (U)"/>
+                    <Input Title="Seuil U" Change={setseuil_U} Value={seuil_U} Length={6} Placeholder="Entrez Valeur"/>
                     <View style={styles.container2}>
                         <Seuil Title="Injecter" Value={inj_U.toFixed(2)}/>
                     </View>
@@ -79,11 +90,11 @@ const styles = StyleSheet.create({
         backgroundColor: colors.background,
     },
     container1: {
+        display: 'flex',
         flexDirection: 'row',
-        borderTopWidth: 5,
-        borderColor: colors.borderinput,
-        borderRadius: 10,
         borderBottomWidth: 1,
+        borderColor: colors.bordercontainer,
+        borderRadius: 10,
     },
     container2: {
         display: 'flex',
@@ -122,35 +133,29 @@ const styles = StyleSheet.create({
     },
     text1: {
         width: 150,
-        margin: 4,
-        paddingHorizontal: 4,
+        // margin: 0,
+        // paddingHorizontal: 0,
         color: colors.texte,
-        // borderColor: colors.borderinput,
-        // borderWidth: 1,
-        // borderRadius: 25,
         textAlign: 'center',
-        textAlignVertical: 'center',
+        // textAlignVertical: 'center',
     },
     text2: {
-        // width: 70,
         fontSize: 20,
-        paddingHorizontal: 8,
+        paddingBottom: 8,
+        paddingTop: 8,
         color: colors.texte,
         textAlignVertical: 'center',
     },
     text3: {
         width: 120,
-        marginLeft: 20,
         color: colors.texte,
     },
     text4: {
-        width: 150,
+        width: 120,
         height: 45,
-        paddingLeft: 10,
-        borderWidth:1,
         color: colors.texte,
         borderColor: colors.borderinput,
-        borderWidth: 1,
+        borderWidth: 0,
         borderRadius: 25,
         textAlignVertical: 'center',
     },
